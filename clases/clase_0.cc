9e87b941b93f6a83925391c85f7edd88a87d519b
00157825 <00157825uca.edu.sv>
2025-08-13 11:46:51 -0600

    area circulo

--- clases/clase_0.cc
+++ clases/clase_0.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 
+float area_circulo (float x);
+
 int main()
 {
+
+    float radio;
+
+    std :: cout << "Ingrese el radio de su circulo: ";
+    std :: cin >> radio;
+    std :: cout << "/n";
+
+    std :: cout << "El area de su circulo es: "<< area_circulo(radio)<< " m^2";
+
 
     
     return 0;
 }
+
+float area_circulo (float x)
+{
+    return 3.1416 * (x * x);
+};
