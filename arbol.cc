86f3c6479d14db733b227aade49d0f99910e7f3e
00157825 <00157825@uca.edu.sv>
2025-11-05 12:36:51 -0600

    :sparkles: Feature: added new files

--- arbol.cc
+++ arbol.cc
@@ -20,15 +20,33 @@ void insertar_nodo(Nodo *&abb, const Tiobe &indice);
 
 void RecorridoInorden(Nodo *abb);
 
+Nodo *BuscarMenor(Nodo *abb);
+
+Nodo *BuscarMayor(Nodo *abb);
+
+void buscar_nodo(Nodo *&abb, float puntaje_busqueda, Nodo *&padre);
+
+void BorrarNodo(Nodo *&abb, float puntaje_buscado);
+
 struct Tiobe SolicitarInformacion();
 
 int main()
 {
+    int opc;
     struct Nodo *abb = nullptr;
-    Tiobe informacion = SolicitarInformacion();
-    crear_nodo(informacion);
-    insertar_nodo(abb, informacion);
+    do
+    {
+        Tiobe indice_tiobe = SolicitarInformacion();
+        crear_nodo(indice_tiobe);
+        insertar_nodo(abb, indice_tiobe);
+
+        std::cout << "¿Desea agregar otro dato? \n 1. Si \n 2. No\n";
+        std::cin >> opc;
+
+    } while (opc == 1);
+
     RecorridoInorden(abb);
+    BorrarNodo(abb, 15);
 
     return 0;
 }
@@ -49,7 +67,7 @@ void insertar_nodo(Nodo *&abb, const Tiobe &indice)
         abb = crear_nodo(indice);
         return;
     }
-    //comparativa con la raiz ABB
+    // comparativa con la raiz ABB
 
     if (indice.puntaje < abb->indice_tiobe.puntaje)
         insertar_nodo(abb->izquierdo, indice);
@@ -59,15 +77,15 @@ void insertar_nodo(Nodo *&abb, const Tiobe &indice)
 
 struct Tiobe SolicitarInformacion()
 {
-    struct Tiobe informacion_indice;
+    struct Tiobe indice_tiobe;
     std::cin.ignore();
     std::cout << "Ingrese el lenguaje de programacion: ";
-    getline(std::cin, informacion_indice.lenguaje_programacion);
+    getline(std::cin, indice_tiobe.lenguaje_programacion);
 
-    std::cout<<"\nIngrese su puntuaje en el tiobe: ";
-    std::cin >> informacion_indice.puntaje;
+    std::cout << "\nIngrese su puntuaje en el tiobe: ";
+    std::cin >> indice_tiobe.puntaje;
 
-    return informacion_indice;
+    return indice_tiobe;
 }
 
 void RecorridoInorden(Nodo *abb)
@@ -79,4 +97,77 @@ void RecorridoInorden(Nodo *abb)
     RecorridoInorden(abb->izquierdo);
     std::cout << "[" << abb->indice_tiobe.puntaje << " : " << abb->indice_tiobe.lenguaje_programacion << "] ";
     RecorridoInorden(abb->derecho);
+}
+
+void buscar_nodo(Nodo *&abb, float puntaje_busqueda, Nodo *&padre)
+{
+    while (abb != nullptr && abb->indice_tiobe.puntaje != puntaje_busqueda)
+    {
+        padre = abb;
+        if (puntaje_busqueda < abb->indice_tiobe.puntaje)
+            abb = abb->izquierdo;
+        else
+            abb = abb->derecho;
+    }
+}
+
+Nodo *BuscarMenor(Nodo *abb)
+{
+    while (abb && abb->izquierdo != nullptr)
+        abb = abb->izquierdo;
+    return abb;
+}
+
+Nodo *BuscarMayor(Nodo *abb)
+{
+    while (abb && abb->derecho != nullptr)
+    {
+        abb = abb->derecho;
+    }
+    return abb;
+}
+
+// Borrar un nodo
+void BorrarNodo(Nodo *&abb, float puntaje_buscado)
+{
+    Nodo *padre = nullptr;
+    Nodo *aux = abb;
+    buscar_nodo(aux, puntaje_buscado, padre);
+    // Si es nulo
+    if (!aux)
+        return;
+
+    // Si es un nodo hoja
+    if (!aux->izquierdo && !aux->derecho)
+    {
+        if (aux == abb)
+            abb = nullptr;
+        else if (padre->izquierdo == aux)
+            padre->izquierdo = nullptr;
+        else
+            padre->derecho = nullptr;
+        delete aux;
+    }
+    // Si es un nodo con dos hijos
+    else if (aux->izquierdo && aux->derecho)
+    {
+        Nodo *predecesor = BuscarMayor(aux->derecho);
+        aux->indice_tiobe = predecesor->indice_tiobe;
+        BorrarNodo(aux->derecho, predecesor->indice_tiobe.puntaje);
+    }
+    // Si es un nodo con un solo hijo
+    else
+    {
+        Nodo *hijo = ((aux->izquierdo) ? aux->izquierdo : aux->derecho);
+        if (aux != abb)
+        {
+            if (aux == padre->izquierdo)
+                padre->izquierdo = hijo;
+            else
+                padre->derecho = hijo;
+        }
+        else
+            abb = hijo;
+        delete aux;
+    }
 }
