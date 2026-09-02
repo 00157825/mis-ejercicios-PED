5a0baa8da0d70b9a494f6ad24d350fa3a98d9bf6
00157825 <00157825@uca.edu.sv>
2026-09-02 12:01:36 -0600

    nuevo commit

--- passTypes.cc
+++ passTypes.cc
@@ -1,26 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
-void cambiarValorReferencia(int *&valor)
-{
-    *valor = 0; // Cambiamos el valor de la variable original a través de la referencia
+//Paso por valor
+void cambiarValor(int a){
+    a = 20;
 }
 
-void cambiarValor(int &a)
-{
-    a = 10; // Cambiamos el valor de la variable original a través de la referencia
+//Paso por referencia aaaaaa
+// Todo lo que pase a "b" le pasara tambien a la variable "a"
+void cambiarValorReferencia(int& b){
+    b = 0;
+} 
+
+//Paso por referencia de punteros
+//int*& es la direccion de memoria de un puntero
+void cambiarValorReferenciaPtr(int*& a){
+    a = nullptr;
 }
 
-void cambiarValorPuntero(int *ptr)
-{
-    *ptr = 20; // Cambiamos el valor de la variable original a través del puntero
-}
-int main()
-{
+int main(){
     int a = 10;
-    int *ptr = &a;                                    // Creamos un puntero que apunta a la variable a
-    cout << "Valor original de ptr: " << ptr << endl; // Imprimimos el valor original de a
-    cambiarValorReferencia(ptr);                      // Llamamos a la función que cambia el valor de a
+    int *ptr = &a;
 
-    return 0;
+    /*
+    cout<<"a: "<<a<<endl; // 10
+    cambiarValorReferencia(a);
+    cout<<"a luego de ejecutar funcion: "<<a<<endl; 
+    */
+
+    std::cout<<"ptr: "<<ptr<<endl; // 10
+    cambiarValorReferenciaPtr(ptr);
+    std::cout<<"ptr luego de ejecutar funcion: "<<ptr<<endl; 
+    
+    return 0;   
 }
